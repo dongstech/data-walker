07b1442b97ea65aead3f7f30d7730a96d96f6482
Frank Dong <frankdong80@gmail.com>
2021-09-04 17:53:05 +0800

    #11 should be able to process the table which has 11 rows.

--- data-spider/docparser/docparser.py
+++ data-spider/docparser/docparser.py
@@ -1,6 +1,7 @@
 import json
 from scrapy import Selector
 
+
 class DocParser:
 
     fields = {
@@ -25,28 +26,20 @@ class DocParser:
             result['link'] = f'https://www.cbirc.gov.cn/cn/view/pages/ItemDetail.html?docId={doc_id}&itemId=4113&generaltype=9'
 
             table = selector.xpath(
-                '//div[@class="Section0"]//table | //div[@class="Section1"]//table')
+                '//div[@class="Section0"]//table | //div[@class="Section1"]//table | //div[@class="WordSection1"]//table')
 
             if len(table) > 0:
                 row_number = len(table.xpath('./tr'))
-                # self.log(f'parse_doc table {row_number} rows...')
                 if row_number == 7:
-                    for i in range(row_number):
-                        rowIdx = i+1
-                        value = table.xpath(
-                            f'./tr[{rowIdx}]/td[2]//text()').getall()
-                        value = ''.join(value)
-                        result[self.fields[f'field{rowIdx}']] = value
-                    result[self.fields['field8']] = table.xpath(
-                        './/text()').getall()
+                    result = result | self.parse_table_7_rows(table)
                 elif row_number == 8:
-                    result = result | self.parse_table_8_rows(selector)
+                    result = result | self.parse_table_8_rows(table)
                 elif row_number == 9:
-                    # self.log('call parse_table_9_rows...')
-                    result = result | self.parse_table_9_rows(selector)
+                    result = result | self.parse_table_9_rows(table)
                 elif row_number == 10:
-                    # self.log('call parse_table_10_rows...')
-                    result = result | self.parse_table_10_rows(selector)
+                    result = result | self.parse_table_10_rows(table)
+                elif row_number == 12:
+                    result = result | self.parse_table_12_rows(table)
                 else:
                     for i in range(len(self.fields)):
                         row_idx = i + 1
@@ -61,11 +54,20 @@ class DocParser:
                 result[self.fields['field8']] = origin_content
             yield result
 
-    def parse_table_8_rows(self, selector):
-        # self.log('parse_table_8_rows...')
+    def parse_table_7_rows(self, table):
         result = {}
-        table = selector.xpath(
-            '//div[@class="Section0"]//table | //div[@class="Section1"]//table | //div[@class="WordSection1"]//table')
+        row_number = 7
+        for i in range(row_number):
+            rowIdx = i+1
+            value = table.xpath(
+                f'./tr[{rowIdx}]/td[2]//text()').getall()
+            value = ''.join(value)
+            result[self.fields[f'field{rowIdx}']] = value
+        result[self.fields['field8']] = table.xpath('.//text()').getall()
+        return result
+
+    def parse_table_8_rows(self, table):
+        result = {}
         result[self.fields['field1']] = "".join(table.xpath(
             './tr[1]/td[2]//text()').getall())
 
@@ -86,11 +88,8 @@ class DocParser:
             './/text()').getall())
         return result
 
-    def parse_table_9_rows(self, selector):
-        # self.log('parse_table_9_rows...')
+    def parse_table_9_rows(self, table):
         result = {}
-        table = selector.xpath(
-            '//div[@class="Section0"]//table | //div[@class="Section1"]//table')
         result[self.fields['field1']] = "".join(table.xpath(
             './tr[1]/td[2]//text()').getall())
 
@@ -111,13 +110,10 @@ class DocParser:
             './/text()').getall())
         return result
 
-    def parse_table_10_rows(self, selector):
-        # self.log('parse_table_10_rows...')
+    def parse_table_10_rows(self, table):
         result = {}
-        table = selector.xpath(
-            '//div[@class="Section0"]//table | //div[@class="Section1"]//table')
-        result[self.fields['field1']] = "".join(table.xpath(
-            './tr[1]/td[2]//text()').getall()).strip()
+        result[self.fields['field1']] = "".join(
+            table.xpath('./tr[1]/td[2]//text()').getall()).strip()
 
         result[self.fields['field2']] = "".join(table.xpath('./tr[2]/td[4]//text()').getall() + table.xpath(
             './tr[3]/td[2]//text()').getall() + table.xpath('./tr[4]/td[3]//text()').getall() + table.xpath('./tr[5]/td[2]//text()').getall()).strip()
@@ -132,6 +128,29 @@ class DocParser:
             './tr[9]/td[2]//text()').getall())
         result[self.fields['field7']] = "".join(table.xpath(
             './tr[10]/td[2]//text()').getall())
+        result[self.fields['field8']] = "".join(table.xpath(
+            './/text()').getall())
+        return result
+
+    def parse_table_12_rows(self, table):
+        result = {}
+        
+        result[self.fields['field1']] = "".join(table.xpath(
+            './tr[4]/td[2]//text()').getall()).strip()
+
+        result[self.fields['field2']] = "".join(table.xpath('./tr[5]/td[3]//text()').getall() + table.xpath(
+            './tr[3]/td[3]//text()').getall() + table.xpath('./tr[7]/td[2]//text()').getall())
+
+        result[self.fields['field3']] = "".join(table.xpath(
+            './tr[8]/td[2]//text()').getall())
+        result[self.fields['field4']] = "".join(table.xpath(
+            './tr[9]/td[2]//text()').getall())
+        result[self.fields['field5']] = "".join(table.xpath(
+            './tr[10]/td[2]//text()').getall())
+        result[self.fields['field6']] = "".join(table.xpath(
+            './tr[11]/td[2]//text()').getall())
+        result[self.fields['field7']] = "".join(table.xpath(
+            './tr[12]/td[2]//text()').getall())
         result[self.fields['field8']] = "".join(table.xpath(
             './/text()').getall())
         return result
